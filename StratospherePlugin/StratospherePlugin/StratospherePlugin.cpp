7892cf34a22383f89574c483804683ca04112c7f
Caleb Churness <cchurness@plu.edu>
2022-09-26 12:45:43 -0700

    feat: added notifier that for uploading replays
    currently only finds the file path of a replay

--- StratospherePlugin/StratospherePlugin/StratospherePlugin.cpp
+++ StratospherePlugin/StratospherePlugin/StratospherePlugin.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "StratospherePlugin.h"
 
+using namespace std::filesystem;
 
 BAKKESMOD_PLUGIN(StratospherePlugin, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
 
@@ -9,38 +10,36 @@ std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 void StratospherePlugin::onLoad()
 {
 	_globalCvarManager = cvarManager;
-	//cvarManager->log("Plugin loaded!");
+	
+	cvarManager->registerNotifier("upload_latest_replay", [this](std::vector<std::string> params) {
+		char* userpath = getenv("USERPROFILE");
+		if (userpath == NULL)
+			return;
+		std::string userPathString = std::string(userpath);
 
-	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
-	//	cvarManager->log("Hello notifier!");
-	//}, "", 0);
+		path ReplaysDirectoryPath = path(userPathString + "\\Documents\\My\ Games\\Rocket\ League\\TAGame\\Demos");
+		if (!is_directory(ReplaysDirectoryPath)) {
+			ReplaysDirectoryPath = path(userPathString + "\\OneDrive\\Documents\\My\ Games\\Rocket\ League\\TAGame\\Demos");
+			if (is_directory(ReplaysDirectoryPath))
+				LOG("Directory Found!");
+			else {
+				LOG("Directory not found :(");
+				return;
+			}
+		}
+		else
+			LOG("Directory found!");
 
-	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
-	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
+		directory_iterator ReplaysDirectoryIterator(ReplaysDirectoryPath);
+		if (!ReplaysDirectoryIterator->exists())
+			return;
+		const directory_entry* replayEntry = ReplaysDirectoryIterator.operator->();
+		path replayFilePath = replayEntry->path();
 
-	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
-	//	cvarManager->log("the cvar with name: " + cvarName + " changed");
-	//	cvarManager->log("the new value is:" + newCvar.getStringValue());
-	//});
+		LOG(replayFilePath.c_str());
 
-	//cvar2.addOnValueChanged(std::bind(&StratospherePlugin::YourPluginMethod, this, _1, _2));
-
-	// enabled decleared in the header
-	//enabled = std::make_shared<bool>(false);
-	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
-
-	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
-	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
-	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
-	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&StratospherePlugin::FUNCTION, this, _1, _2, _3));
-	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
-
-
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
-	//	cvarManager->log("Your hook got called and the ball went POOF");
-	//});
-	// You could also use std::bind here
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&StratospherePlugin::YourPluginMethod, this);
+		LOG("Upload replay file here!");
+		}, "uploads latest replay file", PERMISSION_ALL);
 }
 
 void StratospherePlugin::onUnload()
