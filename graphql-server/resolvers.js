977096b992523192e935f2548c11138f09a9d9ed
Chris Holland <chrishollandaise@gmail.com>
2022-09-28 12:06:35 -0700

    feat: add getRandomPlayer, get/addPlayer(s)

--- graphql-server/resolvers.js
+++ graphql-server/resolvers.js
@@ -1,14 +1,49 @@
 const fs = require('fs').promises;
 const path = require('node:path');
 
-const getRandomPlayer = async () => {
+/*
+    Get all players from data/users.txt
+*/
+const getPlayers = async () => {
     const buff = await fs.readFile(path.join('data', 'users.txt'), "utf-8");
-
     const lines = buff.split("\n");
-    const player = lines[Math.floor(Math.random()*lines.length)].trim();
 
+    // clean player names and coerce to schema format before returning
+    return lines.map(line => {
+        const username = line.trim();
+        return {username: username};
+    })
+}
+
+/*
+    Set a new player in data/users.txt
+*/
+const setPlayer = async (username) => {
+    try {
+        await fs.appendFile(path.join('data', 'users.txt'), `\n${username}`);
+    } catch(error){
+        throw error;
+    }
+}
+
+/*
+    Get random player from data/users.txt
+*/
+const getRandomPlayer = async () => {
+    const players = await getPlayers();
+    const player = players[Math.floor(Math.random()*players.length)];
     return player;
 };
+
+/*
+    Get a player with a specific username from data/users.txt
+*/
+const getPlayer = async (username) => {
+    const players = await getPlayers();
+    // find player, else return empty object
+    const player = players.find((player) => player.username === username) || {username: ""};
+    return player;
+}
 
 const replays = [
     {
@@ -27,6 +62,20 @@ const resolvers = {
             const rando = await getRandomPlayer();
             return {...replay, player: rando};
         }),
+        getRandomPlayer: async () => await getRandomPlayer(),
+        getPlayers: async () => await getPlayers(),
+        getPlayer: async (_, {username}) => await getPlayer(username)
+    },
+
+    Mutation: {
+        addPlayer: async (_, {username}) => {
+            try{
+                await setPlayer(username);
+                return true;
+            } catch(_) {
+                return false;
+            }
+        }
     }
 };
 
