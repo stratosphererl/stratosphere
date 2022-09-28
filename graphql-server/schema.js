921456c378d30193666a37f3c3c6a7c9afc44be2
Chris Holland <chrishollandaise@gmail.com>
2022-09-28 12:07:15 -0700

    feat: add Player type and mutation

--- graphql-server/schema.js
+++ graphql-server/schema.js
@@ -3,12 +3,23 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
     type Query {
         replays: [Replay!]!
+        getRandomPlayer: Player!
+        getPlayers: [Player!]!
+        getPlayer(username: String): Player
     }
 
     type Replay {
         id: ID!
         url: String!
-        player: String!
+        player: Player!
+    }
+
+    type Player {
+        username: String!
+    }
+
+    type Mutation {
+        addPlayer(username: String): Boolean
     }
 `;
 
