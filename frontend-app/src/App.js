420cf10357fa703f270c7049bd0fb0b6afa02674
Caleb Churness <cchurness@plu.edu>
2022-10-01 16:44:59 -0700

    feat: Build onto FileInput component

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import { useState } from 'react';
 
 export default function App() {
   return (
@@ -6,41 +7,39 @@ export default function App() {
   );
 }
 
+// Functional component encompassing uploading process
 function FileInput() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  // Called when a new file is selected
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  // Called when UPLOAD REPLAY button is hit
+  const clickHandler = () => {
+    console.log(selectedFile.name)
+    console.log('Replay uploaded!');
+  }
+
   return (
     <div className="flex justify-center">
       <div className="border border-gray-700 rounded mt-3 mb-3 w-96 ">
         <label htmlFor="formFile" className="form-label inline-block mx-2 mb-0 text-gray-700">Select Replay File</label>
-        <input className="form-control
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile" />
+        {/* File input */}
+        <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white 
+        bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 
+        focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile" 
+          onChange={changeHandler}
+        />
         <div className="flex space-x-2 justify-center">
-          <button type="button" className="inline-block 
-          w-full py-2.5 
-          bg-blue-600 
-          text-white 
-          font-medium 
-          text-xs 
-          leading-tight 
-          uppercase  
-          shadow-md 
-          hover:bg-blue-700 hover:shadow-lg 
-          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 
-          active:bg-blue-800 active:shadow-lg 
-          transition 
-          duration-150 
-          ease-in-out">Upload Replay</button>
+          {/* Upload button */}
+          <button type="button" className="inline-block w-full py-2.5 bg-blue-600 text-white font-medium 
+          text-xs leading-tight uppercase  shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
+          focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+          transition duration-150 ease-in-out"
+            onClick={clickHandler}
+          >Upload Replay</button>
         </div>
       </div>
     </div>
