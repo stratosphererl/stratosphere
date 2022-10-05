19eca9d2ac85c1c468977ab7c3978b04554ec94e
Chris Holland <chrishollandaise@gmail.com>
2022-10-04 18:34:36 -0700

    feat(demo client): add error handle, refactor grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState, useEffect } from "react"
 import { useQuery, gql } from "@apollo/client"
 
+// gql query
 const GET_ALL_PLAYERS = gql`
 query Replays {
   getPlayers {
@@ -11,12 +12,17 @@ query Replays {
 `
 
 function LoadingComponent() {
+  const cells = [1,2,3,4,5,6,7,8,9];
   return(
-    <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div className="lds-grid">
+      {
+        // a total of 9 divs that makes up the loading grid
+        cells.map(cell => <div></div>)
+      }
+      </div>
   )
 }
-
-function UserNameComponent() {
+function UserNameComponent({setButtonDisabled, setErrorShown}) {
   const [delay, setDelay] = useState(true);
   // you would otherwise use loading for conditional rendering, but its too fast so can create a fake delay
   const {error, data, loading} = useQuery(GET_ALL_PLAYERS)
@@ -31,21 +37,37 @@ function UserNameComponent() {
   
   if(delay)
     return <LoadingComponent/>
+  
+  
+  if (error) {
+    setButtonDisabled(false)
+    setErrorShown(true)
+    return console.log("Error!")
+  }
 
-  console.log(data.getPlayers)    
   return data.getPlayers.map((player: any) => <p>{player.username}</p>)
 }
 
 function App() {
-  const [listShown, setListShown] = useState(false)
+  const [listShown, setListShown] = useState<boolean>(false)
+  const [errorShown, setErrorShown] = useState<boolean>(false)
+
   return (
     <div className="App">
-      <h1>Stratosphere</h1>
-      <button disabled={listShown} onClick={() => setListShown(true)}>Get Rocket League Users</button>
+      <h1>Stratosphere ⚽🏎️</h1>
+      <button
+       className={errorShown ? "btn-error-trans" : ""} 
+       disabled={listShown} 
+       onClick={() => {
+        if(errorShown)
+          setErrorShown(false)
+        setListShown(true)
+        }}>Get Rocket League Users
+      </button>
       <div style={{visibility: listShown ? 'visible' : 'hidden'}}>
         <h2><strong>Player List:</strong></h2>
         {
-          listShown ? <UserNameComponent></UserNameComponent> : ""
+          listShown ? <UserNameComponent setButtonDisabled={setListShown} setErrorShown={setErrorShown}/> : ""
         }
       </div>
     </div>
