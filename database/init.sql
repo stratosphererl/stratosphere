c294e9190e12271dfdb5245eca082b605e39b201
Chris Holland <chrishollandaise@gmail.com>
2022-10-08 17:36:27 -0700

    chore(db): change default pass

--- database/init.sql
+++ database/init.sql
@@ -3,6 +3,8 @@ ALTER DATABASE stratosphere OWNER TO postgres;
 
 \connect stratosphere
 
+ALTER USER postgres PASSWORD 'mynewpassword';
+
 CREATE TABLE replay (
 	id		    bigserial	not null	primary key,
 	filename		varchar(120)	not null,
