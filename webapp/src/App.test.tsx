c6e2b56131b1cfce7e031c19fb0d6b34322910c4
Caleb Churness <cchurness@plu.edu>
2022-11-07 21:02:04 -0800

    feat: Create new webapp using vite

--- webapp/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Select Replay File Text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Select Replay File/i);
-  expect(linkElement).toBeDefined();
-});
