5f7edd07e276338683ad7ec6e8856fc869ce75ec
Caleb Churness <cchurness@plu.edu>
2022-11-07 21:57:27 -0800

    chore: Move code from old frontend branch
    Code from the frontend branch is mostly outdata
    but there are still some important pieces
    that haven't been pulled to main. That's
    why I copied files from there rather than main.

---
+++ webapp/src/gql-components/usernames.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { LoadingAnimation } from "../components/loading";
+
+function UserNameComponent() {
+    const [delay, setDelay] = useState(true);
+    const {error, data, loading} = useQuery(GET_ALL_PLAYERS);
+  
+    useEffect(() => {
+      setTimeout(() => {
+         console.log("This is a mock delay announcement")
+         setDelay(false)
+      }, 3000)
+    }, []);
+  
+    // Change delay to loading later
+    if (delay)
+      return <LoadingAnimation />
+  
+    if (error)
+      return <p>Error you suck!</p>
+  
+    let id = 0;
+    return (
+      <div>
+        <div className="text-center">
+        <h1 className="text-5xl text-blue-600 font-bold mt-0 mb-6">Player Names</h1>
+          {
+            data?.getPlayers?.map((player: any) => <p className="text-gray-700 font-semibold" key={++id}>{player.username}</p>)
+          }
+        </div>
+      </div>
+    );
+  }
