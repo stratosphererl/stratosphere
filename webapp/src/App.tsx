8ef53255944cb12d49bf6ec22b3b1962bc8ed51e
Caleb Churness <cchurness@plu.edu>
2022-11-07 23:59:20 -0800

    chore: Moved SceneManager to current branch

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,13 +2,15 @@ import './App.css'
 import HeatmapFromGQL from './gql-components/HeatmapFromGQL'
 // import ParseReplay from './gql-components/ParseReplay'
 import { UserNameComponent } from './gql-components/usernames'
+import ReplayCanvas from './replayViewer/ReplayCanvas'
 
 function App() {
   return (
     <div className="App">
       <header className="justify-center">
         <p className="text-center font-bold text-6xl">Hello World</p>
-        <HeatmapFromGQL/>
+        <HeatmapFromGQL />
+        <ReplayCanvas />
         <UserNameComponent />
       </header>
     </div>
---
+++ webapp/src/replayViewer/SceneManager.ts
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+
+export default class SceneManager {
+    private clock: THREE.Clock;
+    public readonly scene: THREE.Scene;
+
+    private camera: THREE.PerspectiveCamera;
+    private fov: number;
+    
+    private renderer: THREE.WebGLRenderer;
+
+    private controls: OrbitControls;
+    private stats: Stats;
+
+    constructor(private canvasID: string) {
+        this.clock = new THREE.Clock();
+        this.scene = new THREE.Scene();
+
+        const canvas = document.getElementById(canvasID);
+        if (canvas == null)
+            throw new Error("this shit not bussin no cap fr");
+        this.renderer = new THREE.WebGLRenderer({
+            canvas,
+            antialias: true,
+        });
+
+        // const width = window.innerWidth;
+        // const height = window.innerHeight;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        
+        this.camera = new THREE.PerspectiveCamera(
+            this.fov = 45,
+            width / height,
+            1,
+            1000,
+        );
+        this.camera.position.z = 96;
+
+        this.renderer.setSize(width, height);
+        document.body.appendChild(this.renderer.domElement);
+
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+        this.stats = Stats();
+        document.body.appendChild(this.stats.dom);
+
+        // Likely to be moved later so we can better control over lighting
+        const ambientLight = new THREE.AmbientLight(0xffffff, .5);
+        ambientLight.castShadow = true;
+        this.scene.add(ambientLight);
+
+        // Same comment from before applies here
+        const spotLight = new THREE.SpotLight(0xffffff, 1);
+        spotLight.castShadow = true;
+        spotLight.position.set(0, 64, 32);
+        this.scene.add(spotLight);
+    }
+
+    public animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+        this.render();
+        this.stats.update();
+        this.controls.update();
+    }
+
+    public render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+}
