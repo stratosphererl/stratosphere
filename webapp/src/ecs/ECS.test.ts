2a6b219874691ecdd78902c219a5ad4ed4d30edd
Caleb Churness <cchurness@plu.edu>
2022-11-11 13:37:11 -0800

    chore: Split component test into two
    Components can be labeled using strings
    and numbers, it's important to make sure
    both ways work.

--- webapp/src/ecs/ECS.test.ts
+++ webapp/src/ecs/ECS.test.ts
@@ -1,7 +1,28 @@
 import { expect, test } from '@jest/globals';
 import { addComponent, createEntity, getComponentData } from './ECS';
 
-test('Creates a new alterable component', () => {
+test('Test String Identifier', () => {
+    // Create the component
+    const defaultState = {
+        hp: 100,
+        stamina: 100,
+    }
+    addComponent('Player', {
+        name: 'Player',
+        description: 'Players health and stamina',
+        defaultState
+    });
+
+    // Create entity
+    // Use 'Player' as id
+    const playerOne = createEntity(['Player']);
+
+    // Check that the saved component data is the same data, but not the same object
+    expect(getComponentData('Player', playerOne)).not.toBe(defaultState);
+    expect(getComponentData('Player', playerOne)).toEqual(defaultState);
+});
+
+test('Test Number Identifier', () => {
     // Create the component
     const defaultState = {
         hp: 100,
@@ -16,13 +37,8 @@ test('Creates a new alterable component', () => {
     // Create entity
     // Use 'Player' as id
     const playerOne = createEntity(['Player']);
-    expect(getComponentData('Player', playerOne)).not.toBe(defaultState);
-    expect(getComponentData('Player', playerOne)).toEqual(defaultState);
-    // Use playerComponentID as id
-    const playerTwo = createEntity([playerComponentID]);
-    expect(getComponentData(playerComponentID, playerTwo)).not.toBe(defaultState);
-    expect(getComponentData(playerComponentID, playerTwo)).toEqual(defaultState);
 
-    expect(getComponentData('Player', playerOne)).not.toBe(getComponentData('Player', playerTwo));
-
+    // Check that the saved component data is the same data, but not the same object
+    expect(getComponentData(playerComponentID, playerOne)).not.toBe(defaultState);
+    expect(getComponentData(playerComponentID, playerOne)).toEqual(defaultState);
 });
