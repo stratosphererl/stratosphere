0769a26a0359339cfaa3555316c177aefd827ccd
Caleb Churness <cchurness@plu.edu>
2022-11-17 13:39:54 -0800

    fix: Close replay viewer when switching tabs

--- webapp/src/replayViewer/ReplayCanvas.tsx
+++ webapp/src/replayViewer/ReplayCanvas.tsx
@@ -1,20 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import * as THREE from "three";
 import SceneManager from "./SceneManager";
 
-export default function ReplayCanvas() {
+export default function ReplayCanvas({width = 1280, height = 720}: {width?: number, height?: number}) {
+    const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const sm = new SceneManager('replay-canvas');
+        const sm = new SceneManager(width, height);
+        if (ref.current)
+            ref.current.appendChild(sm.renderer.domElement);
         sm.animate();
         
         const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
         const boxMaterial = new THREE.MeshNormalMaterial();
         const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
         sm.scene.add(boxMesh);
-    })
+
+        return () => {if (ref.current) ref.current.removeChild(sm.renderer.domElement)};
+    }, [])
 
     return (
-        <canvas id="replay-canvas" width={1280} height={720} className="m-auto" />
+        <div ref={ref} />
     )
 }
