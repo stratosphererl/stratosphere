17f2b70213c4d2c79e9a8eff1617eea74e2913c4
Chris Holland <chrishollandaise@gmail.com>
2022-11-16 18:27:17 -0800

    chore(webapp): removed footer temporarily

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer () {
     return (
-    <footer className="p-4 bg-white md:p-6 bg-zinc-800 fixed inset-x-0 bottom-0 shadow md:flex md:items-center md:justify-betwee">
+    <footer className="p-4 bg-white md:p-6 bg-zinc-800 inset-x-0 bottom-0 shadow md:flex md:items-center md:justify-betwee">
     <span className="text-sm text-gray-500 text:center dark:text-gray-400 inline-block mx-auto">© 2022 <a href="https://github.com/stratosphererl/stratosphere" className="hover:underline text-center">Stratosphere™</a>. All Rights Reserved.
     </span>
     </footer>
