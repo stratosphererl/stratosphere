7e31a4cac79a8f1d636d84567a28ad0955b50f8c
Chris Holland <chrishollandaise@gmail.com>
2023-01-07 11:52:34 -0800

    feat(parser): parse replay and return analysis

--- parser/app.py
+++ parser/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, request, flash
 from werkzeug.utils import secure_filename
-import json
 import os
+import carball
+from carball.json_parser.game import Game
+from carball.analysis.analysis_manager import AnalysisManager
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.isdir(UPLOAD_FOLDER):
@@ -11,16 +13,25 @@ ALLOWED_EXTENSIONS = {'replay'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.secret_key = os.urandom(24)
+    
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 ## make a method called parser Read json file from uploads and return a json object
-def parser(filename):
-    with open(os.path.join("uploads", "replay.json")) as f:
-        return json.load(f)
+def parser(path):
+        
+    _json = carball.decompile_replay(path)
+    game = Game()
+    game.initialize(loaded_json=_json)
+
+    analysis_manager = AnalysisManager(game)
+    analysis_manager.create_analysis()
+
+    return analysis_manager.get_json_data()
+    
 
 @app.route("/")
 def home():
@@ -37,8 +48,10 @@ def parse():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            print("file is allowed")
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return parser(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            return parser(path)
     
     return parser(os.path.join(app.config['UPLOAD_FOLDER'], "replay.json"))
