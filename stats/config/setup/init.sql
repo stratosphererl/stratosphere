6651b8bca6c4bb50fd88fd1ef8680c4c037f52fa
Matt <jugovimm@plu.edu>
2023-02-18 15:38:49 -0800

    Modified init.sql to three relations: replays_by_rank, replays_by_season, users_by_platform

--- stats/config/setup/init.sql
+++ stats/config/setup/init.sql
@@ -1,15 +1,15 @@
--- CREATE DATABASE statsdb;
--- ALTER DATABASE statsdb OWNER TO postgres;
--- \connect statsdb
+-- May want to create SQL scalar function to create tables in less code
+-- https://www.ibm.com/docs/en/db2/11.5?topic=functions-creating-sql-scalar
 
-CREATE TABLE rank (
-	num 	smallserial,
+-- RELATIONS FOR REPLAYS
+-- replays_by_rank
+CREATE TABLE replays_by_rank (
+    num 	smallserial,
 	name 	text,
     count   int,
 	PRIMARY KEY (num));
 
--- Required ranking data
-INSERT INTO rank (num, name, count) VALUES
+INSERT INTO replays_by_rank (num, name, count) VALUES
 	(0,'Unranked',0),
 	(1,'Bronze1',0),
     (2,'Bronze2',0),
@@ -34,12 +34,45 @@ INSERT INTO rank (num, name, count) VALUES
     (21,'GrandChamp3',0),
 	(22,'SupersonicLegend',0);
 
-CREATE TABLE platform (
+-- replays_by_season
+CREATE TABLE replays_by_season (
     num     smallserial,
     name    text,
     count   int,
     PRIMARY KEY (num));
 
-INSERT INTO platform (num, name, count) VALUES
+INSERT INTO replays_by_season (num, name, count) VALUES
+    (0,'LS1',0),
+    (1,'LS2',0),
+    (2,'LS3',0),
+    (3,'LS4',0),
+	(4,'LS5',0),
+    (5,'LS6',0),
+    (6,'LS7',0),
+    (7,'LS8',0),
+	(8,'LS9',0),
+    (9,'LS10',0),
+    (10,'LS11',0),
+    (11,'LS12',0),
+	(12,'LS13',0),
+    (13,'LS14',0),
+    (14,'FS1',0),
+    (15,'FS2',0),
+	(16,'FS3',0),
+    (17,'FS4',0),
+    (18,'FS5',0),
+    (19,'FS6',0),
+	(20,'FS7',0),
+    (21,'FS8',0);
+
+-- RELATIONS FOR USERS
+-- users_by_platform
+CREATE TABLE users_by_platform (
+    num     smallserial,
+    name    text,
+    count   int,
+    PRIMARY KEY (num));
+
+INSERT INTO users_by_platform (num, name, count) VALUES
     (0,'STEAM',0),
     (1,'EPIC',0);
