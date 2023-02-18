8af64eb71052d58ef1632978d540f069887b2d14
Chris Holland <chrishollandaise@gmail.com>
2023-02-18 11:00:28 -0800

    fix typo

--- service/config/envs.py
+++ service/config/envs.py
@@ -4,7 +4,7 @@ ENV_PATH = ".env"
 dotenv.load_dotenv(dotenv_path=ENV_PATH)
 
 # TODO: Add your environment variables names here
-SERVICE_VAR_NAME = "SERICE_NAME"
+SERVICE_VAR_NAME = "SERVICE_NAME"
 SERVICE_VAR_PORT_NAME = "SERVICE_PORT"
 
 DB_VAR_HOST_NAME = "DB_HOST"
