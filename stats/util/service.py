4eeb28db2cde5d1e7008f8b5519a5ad812796c40
Chris Holland <chrishollandaise@gmail.com>
2023-02-27 16:41:55 -0800

    fix(mock): pass mock as object to Service

--- stats/util/service.py
+++ stats/util/service.py
@@ -1,6 +1,10 @@
 from config import database
 import abc
+from unittest.mock import MagicMock
 
 class Service(abc.ABC):
-    def __init__(self, db=database.Database()):
-        self.db = db
+    def __init__(self, db=database.Database):
+        if not isinstance(db, MagicMock):
+            self.db = db()
+        else:
+            self.db = db
