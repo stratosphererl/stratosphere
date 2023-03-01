ecb7034c08f3454ccddab5a0c805d5c5b411cc61
Matt <jugovimm@plu.edu>
2023-03-01 12:17:30 -0800

    Refactored  database tests into Database_unit_test.py

--- stats/tests/Database_unit_test.py
+++ stats/tests/Database_unit_test.py
@@ -8,55 +8,63 @@ from config.database import Database
 #         yield mock
 
 def test_get_replay_count_all():
-    assert db.get_replay_count_all() == 56 # Total number of replays == 56
+    EXPECTED_VALUES = [56]
+    assert db.get_replay_count_all() == EXPECTED_VALUES[0] # Total count == 56
 
 def test_get_replay_count_arena():
-    assert db.get_replay_count_arena(3) == 7 # Number of replays with arena num 3 == 7
-    assert db.get_replay_count_arena(27) == 41 # Number of replays with arena num 27 == 41
-    assert db.get_replay_count_arena(3) + db.get_replay_count_arena(27) == 48 # Adding the two above values results in value == 48
+    EXPECTED_VALUES = [7, 41, 48]
+    assert db.get_replay_count_arena(3) == EXPECTED_VALUES[0] # Arena num 3 count == 7
+    assert db.get_replay_count_arena(27) == EXPECTED_VALUES[1] # Arena num 28 count == 41
+    assert db.get_replay_count_arena(3) + db.get_replay_count_arena(27) == EXPECTED_VALUES[2] # Adding previous two == 48
 
 def test_get_replay_count_duration():
-    assert db.get_replay_count_duration(min_duration = 0, max_duration = 1230) == 37 # Number of replays in duration relation == 37
-    assert db.get_replay_count_duration(min_duration = 30, max_duration = 1230) == 25 # Number of replays with duration >= 30s == 25
-    assert db.get_replay_count_duration(min_duration = 0, max_duration = 360) == 31 # Number of replays with duration < 360s == 31
-    assert db.get_replay_count_duration(min_duration = 0, max_duration = 390) == 33 # Number of replays with duration < 390s == 33
-    assert db.get_replay_count_duration(min_duration = 30, max_duration = 600) == 21 # Number of replays with 30s <= duration < 600s == 21
-    assert db.get_replay_count_duration(min_duration = 360, max_duration = 300) == 0 # Min > Max, should return 0 always
+    EXPECTED_VALUES = [37, 25, 31, 33, 21, 0]
+    assert db.get_replay_count_duration(min_duration = 0, max_duration = 1230) == EXPECTED_VALUES[0] # Total count == 37
+    assert db.get_replay_count_duration(min_duration = 30, max_duration = 1230) == EXPECTED_VALUES[1] # Duration > 30s count == 25
+    assert db.get_replay_count_duration(min_duration = 0, max_duration = 360) == EXPECTED_VALUES[2] # Duration < 360s count == 31
+    assert db.get_replay_count_duration(min_duration = 0, max_duration = 390) == EXPECTED_VALUES[3] # Duration  390s count == 33
+    assert db.get_replay_count_duration(min_duration = 30, max_duration = 600) == EXPECTED_VALUES[4] # Duration 30s <= d < 600s == 21
+    assert db.get_replay_count_duration(min_duration = 360, max_duration = 300) == EXPECTED_VALUES[5] # min > max, returns 0
 
 def test_get_replay_count_rank():
-    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 22) == 56 # Should return the number of all replays which == 56
-    assert db.get_replay_count_rank(low_rank_num = 2, high_rank_num = 22) == 53 # Returns number of replays Bronze II and up which == 53
-    assert db.get_replay_count_rank(low_rank_num = 17, high_rank_num = 22) == 15 # Returns number of replays Champ II and up which == 15
-    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 13) == 33 # Returns number of replays Diamond I and below which == 33
-    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 20) == 51 # Returns number of replays GrandChamp II and below which == 51
-    assert db.get_replay_count_rank(low_rank_num = 10, high_rank_num = 12) == 8 # Returns number of all Platinum replays which == 8
-    assert db.get_replay_count_rank(low_rank_num = 15, high_rank_num = 15) == 4 # Returns number of all Diamond III replays which == 4
-    assert db.get_replay_count_rank(low_rank_num = 10, high_rank_num = 9) == 0 # low_rank_num > high_rank_num, returns 0
+    EXPECTED_VALUES = [56, 53, 15, 33, 51, 8, 4, 0]
+    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 22) == EXPECTED_VALUES[0] # Total count == 56
+    assert db.get_replay_count_rank(low_rank_num = 2, high_rank_num = 22) == EXPECTED_VALUES[1] # Bronze II and up count == 53
+    assert db.get_replay_count_rank(low_rank_num = 17, high_rank_num = 22) == EXPECTED_VALUES[2] # Champ II and up count == 15
+    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 13) == EXPECTED_VALUES[3] # Diamond I and below count == 33
+    assert db.get_replay_count_rank(low_rank_num = 0, high_rank_num = 20) == EXPECTED_VALUES[4] # GrandChamp II and below count == 51
+    assert db.get_replay_count_rank(low_rank_num = 10, high_rank_num = 12) == EXPECTED_VALUES[5] # Any platinum rank count == 8
+    assert db.get_replay_count_rank(low_rank_num = 15, high_rank_num = 15) == EXPECTED_VALUES[6] # Diamond III count = 4
+    assert db.get_replay_count_rank(low_rank_num = 10, high_rank_num = 9) == EXPECTED_VALUES[7] # low > high, returns 0
 
 def test_get_replay_count_season():
-    assert db.get_replay_count_season(low_season_num = 0, high_season_num = 21) == 22 # Should return the number of all replays in relation which == 22
-    assert db.get_replay_count_season(low_season_num = 1, high_season_num = 21) == 15 # Returns number of replays from seasons Legacy 2 and onwards which == 15
-    assert db.get_replay_count_season(low_season_num = 11, high_season_num = 21) == 11 # Returns the number of replays from season Legacy 12 and onwards which == 11
-    assert db.get_replay_count_season(low_season_num = 14, high_season_num = 21) == 10 # Returns the number of replays from any Free-to-Play (FtP) season
-    assert db.get_replay_count_season(low_season_num = 19, high_season_num = 21) == 4 # Returns the number of replays from FtP 6 and onwards which == 4
-    assert db.get_replay_count_season(low_season_num = 0, high_season_num = 4) == 11 # Returns the number of replays from Legacy 5 and backwards which == 11
-    assert db.get_replay_count_season(low_season_num = 8, high_season_num = 20) == 10 # Returns num replays from Legacy 9 to FtP 7 which == 10
-    assert db.get_replay_count_season(low_season_num = 15, high_season_num = 15) == 6 # Returns num replays from FtP 2 which == 6
+    EXPECTED_VALUES = [22, 15, 11, 10, 4, 11, 10, 6]
+    assert db.get_replay_count_season(low_season_num = 0, high_season_num = 21) == EXPECTED_VALUES[0] # Total count == 2
+    assert db.get_replay_count_season(low_season_num = 1, high_season_num = 21) == EXPECTED_VALUES[1] # Legacy 2 and onwards count == 15
+    assert db.get_replay_count_season(low_season_num = 11, high_season_num = 21) == EXPECTED_VALUES[2] # Legacy 12 and onwards count == 11
+    assert db.get_replay_count_season(low_season_num = 14, high_season_num = 21) == EXPECTED_VALUES[3] # Free-to-Play count == 10
+    assert db.get_replay_count_season(low_season_num = 19, high_season_num = 21) == EXPECTED_VALUES[4] # FtP 6 and onwards count == 4
+    assert db.get_replay_count_season(low_season_num = 0, high_season_num = 4) == EXPECTED_VALUES[5] # Legacy 5 and backwards count == 11
+    assert db.get_replay_count_season(low_season_num = 8, high_season_num = 20) == EXPECTED_VALUES[6] # Legacy 9 to FtP 7 count == 10
+    assert db.get_replay_count_season(low_season_num = 15, high_season_num = 15) == EXPECTED_VALUES[7] # FtP 2 count == 6
 
 def test_get_user_count_all():
-    assert db.get_user_count_all() == 74 # Should return the number of all users which == 74
+    EXPECTED_VALUES = [74]
+    assert db.get_user_count_all() == EXPECTED_VALUES[0] # Total count == 74
 
 def test_get_user_count_platform():
-    assert db.get_user_count_platform(0) == 41 # Returns the number of our users from the Steam platform
-    assert db.get_user_count_platform(1) == 33 # Returns the number of our users from the Epic Games platform
+    EXPECTED_VALUES = [41, 33]
+    assert db.get_user_count_platform(0) == EXPECTED_VALUES[0] # Steam user count == 41
+    assert db.get_user_count_platform(1) == EXPECTED_VALUES[1] # Epic Games user count == 33
 
 def test_get_user_count_rank():
-    assert db.get_user_count_rank(low_rank_num = 0, high_rank_num = 22) == 48 # Should return the total number of users which == 48
-    assert db.get_user_count_rank(low_rank_num = 1, high_rank_num = 22) == 40 # Returns the number of users Bronze I and above which == 40
-    assert db.get_user_count_rank(low_rank_num = 10, high_rank_num = 22) == 38 # Returns the number of users Platinum I and above which == 38
-    assert db.get_user_count_rank(low_rank_num = 21, high_rank_num = 22) == 19 # Returns the number of users GC III or SSL which == 19
-    assert db.get_user_count_rank(low_rank_num = 22, high_rank_num = 22) == 17 # Returns the number of users who are SSL == 17
-    assert db.get_user_count_rank(low_rank_num = 11, high_rank_num = 11) == 13 # Returns the number of users who are Platinum II which == 13
-    assert db.get_user_count_rank(low_rank_num = 4, high_rank_num = 2) == 0 # low_rank_num > high_rank_num, should return 0
+    EXPECTED_VALUES = [48, 40, 38, 19, 17, 13, 0]
+    assert db.get_user_count_rank(low_rank_num = 0, high_rank_num = 22) == EXPECTED_VALUES[0] # Total count == 48
+    assert db.get_user_count_rank(low_rank_num = 1, high_rank_num = 22) == EXPECTED_VALUES[1] # Bronze I and above count == 40
+    assert db.get_user_count_rank(low_rank_num = 10, high_rank_num = 22) == EXPECTED_VALUES[2] # Plat I and above count == 38
+    assert db.get_user_count_rank(low_rank_num = 21, high_rank_num = 22) == EXPECTED_VALUES[3] # GC III to SSL count == 19
+    assert db.get_user_count_rank(low_rank_num = 22, high_rank_num = 22) == EXPECTED_VALUES[4] # SSL count == 17
+    assert db.get_user_count_rank(low_rank_num = 11, high_rank_num = 11) == EXPECTED_VALUES[5] # Plat II count == 13
+    assert db.get_user_count_rank(low_rank_num = 4, high_rank_num = 2) == EXPECTED_VALUES[6] # low > high, returns 0
 
 db = Database(test = True)
