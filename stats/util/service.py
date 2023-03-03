6c863cb86fd8ebf8542b4137bef92ac628262741
Matt <jugovimm@plu.edu>
2023-03-03 12:41:56 -0800

    Fixed bug causing browser access of endpoints not to work

--- stats/util/service.py
+++ stats/util/service.py
@@ -5,6 +5,6 @@ from unittest.mock import MagicMock
 class Service(abc.ABC):
     def __init__(self, db=database.Database):
         if not isinstance(db, MagicMock):
-            self.db = db(test = True) # Added test = True to get Router_integration_test.py working
+            self.db = db(test = False) # Keep as False for browser access of endpoints to work
         else:
             self.db = db
