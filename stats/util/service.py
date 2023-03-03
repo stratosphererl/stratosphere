6ca2cf3518b1862b73b61c9f9aabed04e498b690
Matt <jugovimm@plu.edu>
2023-03-03 13:00:48 -0800

    Successfully got database and router unit tests running again

--- stats/util/service.py
+++ stats/util/service.py
@@ -5,6 +5,6 @@ from unittest.mock import MagicMock
 class Service(abc.ABC):
     def __init__(self, db=database.Database):
         if not isinstance(db, MagicMock):
-            self.db = db(test = False) # Keep as False for browser access of endpoints to work
+            self.db = db(test = True)
         else:
-            self.db = db
+            self.db = db(test = False)
