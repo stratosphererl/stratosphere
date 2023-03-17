46c88dde03bb78a6b2faaa54fb766ece9e6a0908
Matt <jugovimm@plu.edu>
2023-03-16 22:25:18 -0700

    Implement header.tsx, made edits for background imaging in wrapper.tsx, added bg for home.tsx, updated index.css, etc., etc.

--- webapp/src/context/ThemeContext.tsx
+++ webapp/src/context/ThemeContext.tsx
@@ -24,3 +24,10 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
