679683191234cfa80ce4c202760d4ac986a9c13d
Matt <jugovimm@plu.edu>
2023-03-18 15:12:03 -0700

    If already logged in, going to /home redirects to /browse/1 (the user's own replays)

--- webapp/src/routes/home.tsx
+++ webapp/src/routes/home.tsx
@@ -1,15 +1,22 @@
-import ThemeSwitch from "../context/switches"
-import { Switch } from '@headlessui/react'
-import { ThemeProvider } from '../context/contexts'
-import { useState, useEffect, useContext } from "react"
+import { useContext } from "react"
+import { Navigate } from "react-router-dom";
 import { UserContext } from "../context/contexts"
 
 export default function Home() {
 
-  return (
-    <div>
-      <div style={{height: 972, width: 1912}}>home.tsx</div>
-      <div style={{height: 1766, width: 1912}}>TEST</div>
-    </div>
-  );
+  const user = useContext(UserContext)
+
+  const userValue = user.user
+  const setUserValue = user.reviseUser
+
+  if (userValue.id !== "0") {
+    return (<Navigate to="/browse/0"/>)
+  } else {
+    return (
+      <div>
+        <div style={{height: 972, width: 1912}}>home.tsx</div>
+        <div style={{height: 1766, width: 1912}}>TEST</div>
+      </div>
+    );
+  }
 }
