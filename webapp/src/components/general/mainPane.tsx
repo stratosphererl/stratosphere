b947f0c0e95c1c94de748fc5a1c682982c92626d
chrishollandaise <chris@chrishollandaise.com>
2023-03-20 13:54:48 -0700

    fix(mainPane): properly center title

--- webapp/src/components/general/mainPane.tsx
+++ webapp/src/components/general/mainPane.tsx
@@ -1,10 +1,13 @@
 import logo from "../../assets/logo/700.png"
 
 export default function MainPane({title, className, children}: {title: string, className: string, children: React.ReactNode}) {
+    const logoSize = 52;
+
     return (
-        <main className={`${className} round glass-outer p-10 m-auto border-white border-[3px]`}>
+        <main className={`${className} round glass-outer p-10 m-auto border-white`}>
             <div className="flex mb-4">
-                <img className="max-h-[52px] max-w-[52px]" src={logo}/>
+                {/* Ignore right margin for other elements*/}
+                <img className={`max-h-[${logoSize}px] max-w-[${logoSize}px] mr-[-${logoSize}px]`} src={logo}/>
                 <h1 className="font-bold m-auto">{title}</h1>
             </div>
             <div>
