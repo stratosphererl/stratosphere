e8150878ef7f9a033c5b7bc0dddb1dce28f24841
Caleb Churness <cchurness@plu.edu>
2023-03-27 16:06:56 -0700

    feat: Match replay page to mock up

--- webapp/src/routes/replay.tsx
+++ webapp/src/routes/replay.tsx
@@ -1,5 +1,33 @@
 import { useParams } from 'react-router-dom';
 
+import { Tab } from '@headlessui/react';
+import { Fragment } from 'react';
+
+import MainPane from '../components/general/mainPane';
+
+const GROUPS: { groupName: string, tabs: { tabName: string, content: JSX.Element}[]}[] = [
+    { groupName: "Group 1", tabs: [
+        { tabName: "Page 1", content: <h1 className="text-center">Content 1</h1>},
+        { tabName: "Page 2", content: <h1 className="text-center">Content 2</h1>},
+        { tabName: "Page 3", content: <h1 className="text-center">Content 3</h1>},    
+    ]},
+    { groupName: "Group 2", tabs: [
+        { tabName: "Page 4", content: <h1 className="text-center">Content 4</h1>},
+        { tabName: "Page 5", content: <h1 className="text-center">Content 5</h1>},
+        { tabName: "Page 6", content: <h1 className="text-center">Content 6</h1>},
+    ]},
+    { groupName: "Group 3", tabs: [
+        { tabName: "Page 7", content: <h1 className="text-center">Content 7</h1>},
+        { tabName: "Page 8", content: <h1 className="text-center">Content 8</h1>},
+        { tabName: "Page 9", content: <h1 className="text-center">Content 9</h1>},
+    ]},
+    { groupName: "Group 4", tabs: [
+        { tabName: "Page 10", content: <h1 className="text-center">Content 10</h1>},
+        { tabName: "Page 11", content: <h1 className="text-center">Content 11</h1>},
+        { tabName: "Page 12", content: <h1 className="text-center">Content 12</h1>}, 
+    ]},
+];
+
 export default function Replay() {
     const params = useParams();
     const regex = /^[A-Z0-9]{32}$/
@@ -10,6 +38,38 @@ export default function Replay() {
     }
 
     return (
-      <div>replay.tsx</div>
+        <MainPane className="mx-[5%]" title="Replay">
+            <div className="p-4 pt-6 glass-inner rounded-2xl text-center h-[200px] flex flex-col justify-center" >
+                <h1 className="break-words">{params.replayid}</h1>
+            </div>
+            <Tab.Group>
+                <div className="flex mt-4 space-x-4" >
+                    <div className="glass-inner rounded-2xl w-[15%] pt-6 p-8" >
+                        <div className="">
+                            <div className="flex flex-col space-y-8">{
+                                GROUPS.map((group) =><div>
+                                    <h1 className="text-center">{group.groupName}</h1>
+                                    <div className="flex flex-col space-y-2">{
+                                        group.tabs.map((tab) => 
+                                            <Tab as={Fragment} >
+                                                {({ selected }) => 
+                                                <button className={
+                                                    `${selected ? 'font-bold bg-stratosphere-blue' : 'border-stratosphere-blue text-stratosphere-blue'}
+                                                    border-solid border-2 p-2
+                                                    rounded-tl-2xl rounded-br-2xl outline-none`
+                                                } style={selected ? {borderColor: "white", color: "white"} : {backgroundColor: "white"}} >{tab.tabName}</button>}
+                                            </Tab>
+                                        )
+                                    }</div>
+                                </div>)}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="glass-inner rounded-2xl w-[85%] pt-6 p-8" >
+                        {GROUPS.map((group) => group.tabs.map((tab) => <Tab.Panel>{tab.content}</Tab.Panel>))}
+                    </div>
+                </div>
+            </Tab.Group>
+        </MainPane>
     );
 }
