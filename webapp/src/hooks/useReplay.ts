7f8c770217aca6ba5faa4bfeba6c6bcbcdecc93d
Caleb Churness <cchurness@plu.edu>
2023-03-28 12:45:46 -0700

    feat: Create a useReplay hook to get data
    I might double back on this decision later, but I prefer it to importing
    mock data directly.

--- webapp/src/hooks/useReplay.ts
+++ webapp/src/hooks/useReplay.ts
@@ -4,19 +4,19 @@ import ResponseDataWrapper from "../data/ResponseDataWrapper";
 import response from "../data/mock/response.json";
 
 export default function useReplay(replayid: string) {
-    // const { data, loading, error } = useQuery(gql`
-    //     query ... {
-    //         ...
-    //     }`
-    // );
+  // const { data, loading, error } = useQuery(gql`
+  //     query ... {
+  //         ...
+  //     }`
+  // );
 
-    const data = response;
-    const loading = false;
-    const error = false;
+  const data = response;
+  const loading = false;
+  const error = false;
 
-    return {
-        data: new ResponseDataWrapper(data),
-        loading: loading,
-        error: error,
-    }
+  return {
+    data: new ResponseDataWrapper(data),
+    loading: loading,
+    error: error,
+  };
 }
