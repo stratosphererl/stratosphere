2558e51392c29f0731d076e41cd197bd4629e18a
Caleb Churness <cchurness@plu.edu>
2023-04-01 15:49:14 -0700

    feat: Create pie chart

--- webapp/src/routes/replay.tsx
+++ webapp/src/routes/replay.tsx
@@ -1,4 +1,10 @@
 import { useParams } from 'react-router-dom';
+import PieChart from '../components/visualizations/pie';
+
+const pieData = [
+    { name: "Steam", value: 12000, color: "rgb(41, 90, 140)" }, // Color doesn't have to be rgb, can also be hex, color name, or css variable
+    { name: "Epic", value: 8000, color: "rgb(50, 50, 50)" },
+]
 
 export default function Replay() {
     const params = useParams();
@@ -10,6 +16,14 @@ export default function Replay() {
     }
 
     return (
-      <div>replay.tsx</div>
+      <div>
+        <h1 className="text-center">Steam and Epic Users</h1>
+        <PieChart // You may need to refresh page to see changes
+            className="w-[10%] m-auto" // Stylings
+            data={pieData} // Data follows the pattern above, you can also look at the pie.tsx file
+            disableTooltip={false} // Optional: Disables tooltip, false by default
+            disableOpacity={false} // Optional: Disables opacity, false by default (opacity is set to 0.9 by default and changes to 1 on hover)
+        />
+      </div>
     );
 }
