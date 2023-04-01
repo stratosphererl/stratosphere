d17a700dbefcca9c90d631cda71f8d33b58ab0f5
Chris Holland <26207348+chrishollandaise@users.noreply.github.com>
2023-03-31 17:49:30 -0700

    pre-Feature: make some fixes to replay service and add basic GQL endpoints (#167)
    * refactor(replay): downgrade mongo, get rid of pydantic models, fix mmr
    
    * remove: other unused deprecated services
    
    * fix the user service, add the ability to download frame data
    
    * add(gql): replay service some bits...
    
    ---------
    
    Co-authored-by: chrishollandaise <chris@chrishollandaise.com>

--- replay/repository/ReplayRepository.py
+++ replay/repository/ReplayRepository.py
@@ -1,5 +1,4 @@
 from util.RepositoryABC import RepositoryABC, MongoRepository
-from schemas.parsed_replay import DetailedReplay
 
 class ReplayRepository(RepositoryABC, MongoRepository):
     def __init__(self, collection):
@@ -16,8 +15,8 @@ class ReplayRepository(RepositoryABC, MongoRepository):
         cursor = self.collection.find()
         return cursor
 
-    def add(self, entity : DetailedReplay):
-        cursor = self.collection.insert_one(entity.dict(by_alias=True))
+    def add(self, entity):
+        cursor = self.collection.insert_one(entity)
         return cursor.inserted_id
 
     def update(self, id, entity):
@@ -25,7 +24,7 @@ class ReplayRepository(RepositoryABC, MongoRepository):
         if not existing_document:
             raise Exception("Replay not found")
         
-        update_result = self.collection.update_one({"_id": id}, {"$set": entity.dict(by_alias=True)})
+        update_result = self.collection.update_one({"_id": id}, {"$set": entity})
         if update_result.modified_count != 1:
             raise Exception("Replay update failed")
         
