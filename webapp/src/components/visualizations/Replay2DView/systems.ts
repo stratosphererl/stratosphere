d4da43c136da28a87a584cfc31fcffef66b4a0e6
chrishollandaise <chris@chrishollandaise.com>
2023-04-15 17:09:39 -0700

    simplify systems

--- webapp/src/components/visualizations/Replay2DView/systems.ts
+++ webapp/src/components/visualizations/Replay2DView/systems.ts
@@ -8,41 +8,18 @@ import { useState } from "react";
 
 let frame = 0;
 
-const interpolation_factor = 1 / 20;
-
 let timeSinceLastFrame = 0;
 let timeBetweenFrames = 1 / 30;
-
-function cubicFactor(
-  y0: number,
-  y1: number,
-  y2: number,
-  y3: number,
-  t: number
-) {
-  const a0 = y3 - y2 - y0 + y1;
-  const a1 = y0 - y1 - a0;
-  const a2 = y2 - y0;
-  const a3 = y1;
-
-  return a0 * t ** 3 + a1 * t ** 2 + a2 * t + a3;
-}
 
 function lerp(start: number, end: number, amt: number) {
   return start + (end - start) * amt;
 }
 
-// function lerp(start: number, end: number, amt: number) {
-//   return start;
-// }
-
 export const CanvasViewSystem = () => {
   const query = useQuery((e) => e.hasAny(Drawable, Transform));
   const canvas = useCanvas();
 
-  return useTimer(
-    timeBetweenFrames,
-    () => {
+  return useTimer(timeBetweenFrames, () => {
     const dt = timeBetweenFrames;
     if (!canvas) return;
     const ctx = canvas.getContext("2d");
@@ -51,38 +28,12 @@ export const CanvasViewSystem = () => {
     ctx.clear();
 
     timeSinceLastFrame += dt;
-    const interpolationPercent = Math.min(1, timeSinceLastFrame / timeBetweenFrames);
-
-    // console.log(timeSinceLastFrame.toFixed(2), dt.toFixed(4), interpolationPercent.toFixed(2));
+    const interpolationPercent = Math.min(
+      1,
+      timeSinceLastFrame / timeBetweenFrames
+    );
 
     query.loop([Drawable, Transform], (e, [drawable, transform]) => {
-      // const drawTransform = {position: new Vector3(10000, 10000, 10000)};
-
-      // if (transform) {
-      //   drawTransform.position.x = lerp(
-      //     transform.position.x,
-      //     transform.nextPosition.x,
-      //     interpolationPercent
-      //   );
-      //   drawTransform.position.y = lerp(
-      //     transform.position.y,
-      //     transform.nextPosition.y,
-      //     interpolationPercent
-      //   );
-      //   drawTransform.position.z = lerp(
-      //     transform.position.z,
-      //     transform.nextPosition.z,
-      //     interpolationPercent
-      //   );
-      // }
-
-      // if (!isFinite(drawTransform.position.z))
-      //   console.log(
-      //     frame,
-      //     drawTransform.position.x.toFixed(2),
-      //     drawTransform.position.y.toFixed(2),
-      //   )
-
       drawable.draw(ctx, transform);
     });
   });
@@ -91,52 +42,25 @@ export const CanvasViewSystem = () => {
 export const TransformSystem = ({ data }: { data: any }) => {
   const query = useQuery((e) => e.hasAll(Transform, Name));
 
-  // const getTillNextFrame = () => {
-  //   if (frame + 1 < data.length) {
-  //     const currFrame = data[frame][7];
-  //     const nextFrame = data[frame + 1][7];
-  //     return nextFrame["time"] - currFrame["time"];
-  //   }
-  //   return timeBetweenFrames;
-  // }
-
-  // const [timeTillNextFrame, setTimeTillNextFrame] = useState(getTillNextFrame());
-
-  // const drawFunction = CanvasViewSystem();
-
-  return useTimer(
-    timeBetweenFrames,
-    () => {
-      timeSinceLastFrame = 0;
-      frame++;
-      // setTimeTillNextFrame(getTillNextFrame());
-      // timeBetweenFrames = timeTillNextFrame;
-      query.loop([Transform, Name], (e, [transform, name]) => {
-        if (frame < data.length) {
-          const game = data[frame];
-          for (let i = 0; i < game.length; i++) {
-            if (game[i].id === name.id) {
-              transform.position.x = game[i].position.x;
-              transform.position.y = game[i].position.y;
-              transform.position.z = game[i].position.z;
-            }
-          }
-          
-          if (frame + 1 < data.length) {
-            const gameNext = data[frame + 1];
-            gameNext.forEach((entity: any) => {
-              if (entity.id === name.id) {
-                transform.nextPosition.x = entity.position.x;
-                transform.nextPosition.y = entity.position.y;
-                transform.nextPosition.z = entity.position.z;
-              }
-            });
+  return useTimer(timeBetweenFrames, () => {
+    timeSinceLastFrame = 0;
+    frame++;
+    query.loop([Transform, Name], (e, [transform, name]) => {
+      if (frame < data.length) {
+        const game = data[frame];
+        for (let i = 0; i < game.length; i++) {
+          if (game[i].id === name.id) {
+            transform.position.x = game[i].position.x;
+            transform.position.y = game[i].position.y;
+            transform.position.z = game[i].position.z;
+            if (game[i]?.rotation?.y !== undefined)
+              transform.rotation.y = game[i].rotation.y;
+            else transform.rotation.y = 0;
           }
         }
-      });
-
-    },
-  );
+      }
+    });
+  });
 };
 
 export const GameTimeSystem = ({ data }) => {
