0ea2033031c203c56082dca266faf526aa12383d
Chris Holland <chrishollandaise@gmail.com>
2023-04-24 22:13:00 -0700

    feat: parser has everything the /browse needs

--- replay/parser/worker.py
+++ replay/parser/worker.py
@@ -2,24 +2,25 @@ from celery import Celery
 import json 
 import shutil
 import os
-from celery.signals import worker_ready
+from celery.signals import worker_ready, worker_shutdown
 import logging
 
 from config.database import collection
 from repository.ReplayRepository import ReplayRepository
-from schemas.parsed_replay import DetailedReplay
 import time
 from datetime import datetime
 
-
+import traceback
 
 # worker container has no parser module so we need to import from the correct location
 if os.getenv("PARSER"):
-    from helper import date2season, filename2map, debug2mmr, mmr2rank
+    from helper import date2season, filename2map, debug2ranks, mmr2rank
     from parsing import carball_parse, boxcars_parse
+    from StratosphereParser import StratosphereParser, ParsingHelper
 else:
-    from parser.helper import date2season, filename2map, debug2mmr, mmr2rank
+    from parser.helper import date2season, filename2map, debug2ranks, mmr2rank
     from parser.parsing import carball_parse, boxcars_parse
+    from parser.StratosphereParser import StratosphereParser, ParsingHelper
     
 
 logger = logging.getLogger(__name__)
@@ -33,11 +34,7 @@ celery = Celery(__name__,
                 backend="redis://redis:6379",
                 broker="redis://redis:6379")
 
-@worker_ready.connect
-def at_start(sender, **k):
-    """
-    Clean up files directory on startup, that were not deleted properly
-    """
+def clean_up_fs():
     for file in os.listdir("./files"):
         if not os.path.isdir(f"./files/{file}"):
             logging.debug(f"Unprocessed file detected. Removing file {file}")
@@ -47,9 +44,36 @@ def at_start(sender, **k):
         if not os.listdir(f"./files/{folder}"):
             logging.debug(f"Empty folder detected. Removing folder {folder}")
             os.rmdir(f"./files/{folder}")
-        elif len(os.listdir(f"./files/{folder}")) < 3:
-            logging.debug(f"Incomplete replay detected. Removing folder {folder}")
+        elif not replay_has_hidden_done_file(f"./files/{folder}"):
+            logging.debug(f"Unprocessed folder detected. Removing folder {folder}")
             shutil.rmtree(f"./files/{folder}")
+
+@worker_ready.connect
+def at_start(sender, **k):
+    """
+    Clean up files directory on startup, that were not deleted properly
+    """
+    clean_up_fs()
+
+@worker_shutdown.connect
+def at_end(sender, **k):
+    """
+    Clean up files directory on shutdown, that were not deleted properly
+    """
+    clean_up_fs()
+
+def add_hidden_done_file(path):
+    """
+    Add a hidden file to the replay folder to indicate that the replay is done processing
+    """
+    with open(f"{path}/.done", "w") as f:
+        f.write("done")
+
+def replay_has_hidden_done_file(path):
+    """
+    Check if the replay has a hidden done file
+    """
+    return os.path.exists(f"{path}/.done")
 
 def generate_task_meta(id, stage, current_stage_num, process_time=None, total_stages=4):
     return {
@@ -62,101 +86,78 @@ def generate_task_meta(id, stage, current_stage_num, process_time=None, total_st
         }
     }
 
+
+def pretty_format_exception(e):
+    return ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+
+
+def persist_data(id, callbacks = []):
+    """
+    Persist replay to file system
+    """
+
+    for callback in callbacks:
+        callback()
+
+    add_hidden_done_file(f"./files/{id}")
+
+def save_frames_if_exists(am):
+    import pandas as pd
+    if not am: return
+    if not isinstance(am.game.frames, pd.DataFrame) or not am.game.frames.empty:
+        ParsingHelper.export_frames_as_zipped_csv(am.get_data_frame(), f"./files/{am.game.id}/{am.game.id}_frames.csv")
+
+def save_replay_to_fs(id, path):
+    if not os.path.exists(f"./files/{id}"):
+        os.mkdir(f"./files/{id}")
+    shutil.copy(path, f"./files/{id}/{id}.replay")
+    os.remove(path)
+
 @celery.task(name="parse", bind=True)
-def parse(self, path):
-    start_time = time.time()
-
+def parse2(self, path):
+    s_time = time.time()
     id = None
-    alreadyExists = False
 
-    logging.debug(f"Processing {path}")
+    # initialize parser
+    sp = StratosphereParser(path)
 
     try:
-        logging.info(f"Parsing raw replay file: {path}")
-        self.update_state(state="PROGRESS", meta=generate_task_meta(self, id, "PARSE", 1))
-        id, raw_replay = boxcars_parse(path)
+        self.update_state(state="PROGRESS", meta=generate_task_meta(id, "PARSE", 1))
+        sp.get_decompile()
+        sp.get_game()
+        
+        id = sp.game.id
 
         try:
             replay_exists_db = bool(repo.get(id))
         except:
             replay_exists_db = False
-        
+
         if os.path.exists(f"./files/{id}") and replay_exists_db:
-            alreadyExists = True
-            raise Exception("Replay already exists")         
-        else:
-            os.mkdir(f"./files/{id}")
-
-        logging.info(f"Analyzing replay file: {path}")
-        self.update_state(state="PROGRESS", meta=generate_task_meta(self, id, "ANALYZE", 2))
-        with open(f"./files/{id}/{id}_boxcars.json", "w") as f:
-            json.dump(raw_replay, f)
+            raise Exception("Replay already exists")
 
         try:
-            parsed_replay = carball_parse(path)
+            self.update_state(state="PROGRESS", meta=generate_task_meta(id, "ANALYZE", 2))
+            sp.perform_analysis()
         except Exception as e:
-            raise Exception(f"Failed to analyze replay: {e}")
+            print(f"Failed to analyze replay: {pretty_format_exception(e)}")
 
-        self.update_state(state="PROGRESS", meta=generate_task_meta(self, id, "STITCH", 3))
-        
-        addMap(parsed_replay)
-
-        addSeason(parsed_replay)
-
-        addUploadDate(parsed_replay)
-
-        addRanks(parsed_replay, raw_replay)
-        
-        with open(f"./files/{id}/{id}_carball.json", "w") as f:
-            json.dump(parsed_replay, f)
-        
-        shutil.copyfile(path, f"./files/{id}/{id}.replay")
-
-        end_time = time.time()
-        execution_time = end_time - start_time
-
-        assert parsed_replay['gameMetadata']['id'] == id
+        self.update_state(state="PROGRESS", meta=generate_task_meta(id, "STITCH", 3))
+        sp.amend_game(sp.game)
 
         try:
-            detailedReplay = DetailedReplay(**parsed_replay)
-            detailedReplay.update_forward_refs()
-            repo.add(detailedReplay)
-            pass
+            json = sp.get_json_data()
+            persist_data(id, callbacks=[lambda: save_replay_to_fs(id, path), lambda: save_frames_if_exists(sp.am)])
+            repo.add(json)
+           
+            e_time = time.time()
+            execution_time = e_time - s_time
+
+            self.update_state(state="SUCCESS", meta=generate_task_meta(id, "SAVE", 4, process_time=f"{execution_time:.3f}s"))
+            
         except Exception as e:
-            logging.error(f"Failed to save replay to database: {e}")
+            print(f"Failed to save replay to database: {pretty_format_exception(e)}")
             raise(e)
         
-        logging.info(f"Replay file {path} saved to database successfully")
-        self.update_state(state="SUCCESS", meta=generate_task_meta(id, "SAVE", 4, process_time=f"{execution_time:.3f}s"))
-
     except Exception as e:
-        # if alreadyExists:
-        #     logging.info(f"Replay file {path} already exists")
-        #     os.remove(path)
-        # else:
-        #     if os.path.exists(f"./files/{id}"):
-        #         shutil.rmtree(f"./files/{id}")
-        #         os.remove(path)
-        raise(e)
-
-def addUploadDate(replay):
-    replay['gameMetadata']['uploadDate'] = str(datetime.now().timestamp())
-
-
-def addMap(replay):
-    replay['gameMetadata']['map'] = dict(filename2map(replay['gameMetadata']['map']))
-
-
-def addSeason(replay):
-    if replay['gameMetadata']['time'].isdigit():
-        time = int(replay['gameMetadata']['time'])
-        replay['gameMetadata']['season'] = dict(date2season(datetime.utcfromtimestamp(time)))
-    else:
-        replay['gameMetadata']['season'] = None
-
-def addRanks(replay, parsedReplay):
-    playlist = replay['gameMetadata']['playlist']
-    # weird bug here, sometimes the ranks aren't processed correctly
-    ranks = debug2mmr(parsedReplay['debug_info'], playlist)
-
-    replay['gameMetadata']['ranks'] = ranks
+        raise Exception(f"Failed to parse replay: {pretty_format_exception(e)}")
