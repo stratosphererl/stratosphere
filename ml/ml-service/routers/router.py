4d54b63dc90f02ed78673cd9eec1b615fbd951e7
Caleb Churness <cchurness@plu.edu>
2023-04-26 19:24:53 -0700

    fix: Fix boxcar dependency

--- ml/ml-service/routers/router.py
+++ ml/ml-service/routers/router.py
@@ -14,3 +14,4 @@ async def predict(file: UploadFile = File(..., description="Replay file to be an
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Uploaded file is not a .replay file.")
 
     return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Hello World"})
+
